a29c27dfbae4fb3088a7715e6a655e45fb4a4f26
NoHara42 <mailned42@gmail.com>
2020-04-15 20:45:51 +0200

    fixed timeline

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css';
 import deflag from './assets/deflag.png';
 import ukflag from './assets/ukflag.png';
+import { edu } from './assets/education.svg';
+import { work } from './assets/workexperience.svg';
 
 function App() {
   return (
@@ -54,10 +56,10 @@ function App() {
               className="vertical-timeline-element--education"
               contentStyle={{ background: '#013535', color: '#fff' }}
               contentArrowStyle={{ borderRight: '7px solid  #013535' }}
-              date="2014 - 2016"
+              date={<div class="date-text">2014 - 2016</div>}
               iconStyle={{ background: '#013535', color: '#fff' }}
-              icon={<img class="flag-size" src={ukflag}></img>}
             >
+              <img class="flag-size" src={ukflag}></img>
               <h3 className="vertical-timeline-element-title">Completed A-Levels at Esher College</h3>
               <h4 className="vertical-timeline-element-subtitle">Surrey, England</h4>
               <p>
@@ -70,12 +72,13 @@ function App() {
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--education"
-              contentStyle={{ background: '#00564b', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #00564b' }}
-              date="2017 - Current"
-              iconStyle={{ background: '#00564b', color: '#fff' }}
-              icon={<img class="flag-size" src={deflag}></img>}
+              contentStyle={{ background: '#013535', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  #013535' }}
+              date={<div class="date-text">2017 - Current</div>}
+              iconStyle={{ background: '#013535', color: '#fff' }}
+            // icon={<svg class="flag-size" src={edu}></svg>}
             >
+              <img class="flag-size" src={deflag}></img>
               <h3 className="vertical-timeline-element-title">Studying Digital Humanities (B.Sc)</h3>
               <h4 className="vertical-timeline-element-subtitle">Leipzig, Germany</h4>
               <p>Digital Humanities is a new field of research, which attempts to combine aspects of the computer sciences with social sciences.</p>
@@ -87,39 +90,47 @@ function App() {
           <VerticalTimeline>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              contentStyle={{ background: '#00564b', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #00564b' }}
-              date="2017 - Current"
-              iconStyle={{ background: '#00564b', color: '#fff' }}
-              icon={<img class="flag-size" src={deflag}></img>}
+              contentStyle={{ background: '#013535', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  #013535' }}
+              date={<div class="date-text">2016 - 2017</div>}
+              iconStyle={{ background: '#013535', color: '#fff' }}
             >
-              <h3 className="vertical-timeline-element-title">Studying Digital Humanities (B.Sc)</h3>
-              <h4 className="vertical-timeline-element-subtitle">Leipzig, Germany</h4>
-              <p>Digital Humanities is a new field of research, which attempts to combine aspects of the computer sciences with social sciences.</p>
+
+              <h3 className="vertical-timeline-element-title">Coworking Community Manager</h3>
+              <h4 className="vertical-timeline-element-subtitle">Basislager Coworking, Leipzig</h4>
+              <p>~ Event management</p>
+              <p>~ Community management. (Offline and Online.)</p>
+              <p>~ Organisation of event formats.</p>
+              <p>~ Facilitation of creative culture principles and values.</p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              contentStyle={{ background: '#00564b', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #00564b' }}
-              date="2017 - Current"
-              iconStyle={{ background: '#00564b', color: '#fff' }}
-              icon={<img class="flag-size" src={deflag}></img>}
+              contentStyle={{ background: '#013535', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  #013535' }}
+              date={<div class="date-text">2016 - 2017</div>}
+              iconStyle={{ background: '#013535', color: '#fff' }}
             >
-              <h3 className="vertical-timeline-element-title">Studying Digital Humanities (B.Sc)</h3>
-              <h4 className="vertical-timeline-element-subtitle">Leipzig, Germany</h4>
-              <p>Digital Humanities is a new field of research, which attempts to combine aspects of the computer sciences with social sciences.</p>
+
+              <h3 className="vertical-timeline-element-title">Student Researcher</h3>
+              <h4 className="vertical-timeline-element-subtitle">Digital City Department, City of Leipzig</h4>
+              <p>~ Automation programming and the digitalisation of bureaucratic processes.</p>
+              <p>~ Project management and organisation of citizen-participation initiatives.</p>
+              <p>~ 'Smart City'-based research.</p>
+
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              contentStyle={{ background: '#00564b', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #00564b' }}
-              date="2017 - Current"
-              iconStyle={{ background: '#00564b', color: '#fff' }}
-              icon={<img class="flag-size" src={deflag}></img>}
+              contentStyle={{ background: '#013535', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  #013535' }}
+              date={<div class="date-text">2016 - 2017</div>}
+              iconStyle={{ background: '#013535', color: '#fff' }}
             >
-              <h3 className="vertical-timeline-element-title">Studying Digital Humanities (B.Sc)</h3>
-              <h4 className="vertical-timeline-element-subtitle">Leipzig, Germany</h4>
-              <p>Digital Humanities is a new field of research, which attempts to combine aspects of the computer sciences with social sciences.</p>
+
+              <h3 className="vertical-timeline-element-title">Back-End Developer</h3>
+              <h4 className="vertical-timeline-element-subtitle">eCommeleon</h4>
+              <p>~ Integrated Walmart marketplace, facilitating large transaction inserts to MySQL databases.</p>
+              <p>~ Built Windows Desktop Applications to facilitate this.</p>
+              <p>~ Responsible for writing technical documentation.</p>
             </VerticalTimelineElement>
           </VerticalTimeline>
         </section>
